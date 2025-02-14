7a844332a83a8c5f48b47197037a08b999e30f4f
sylsylviane <syl.sylviane1@gmail.com>
2025-02-14 11:15:46 -0500

    Créer les livres de départ

---
+++ Backend/data/livresDepart.js
@@ -0,0 +1,64 @@
+const donneesLivres = [
+  {
+    titre: "C'est aussi ça, l'Amérique: portraits d'un pays polarisé",
+    auteur: "Frédéric Arnould",
+    description:
+      "Les États-Unis sont-ils brisés? Armes à feu, avortement, religion, (dés)information, justice… Pourquoi ces enjeux divisent-ils plus que jamais les Américains ? Le journaliste Frédéric Arnould a sillonné les États-Unis et rencontré une foule de citoyens de tous horizons, soulevant – sans jamais la poser – cette éternelle question : « Qu’est-ce qui se passe avec vous, les Américains? » Un portrait instructif et humain de ce pays plein de promesses, qui garantit à ses citoyens une grande liberté et les outils pour la défendre… pour le meilleur et pour le pire.",
+    editeur: "Québec Amérique",
+    isbn: "9782764454992",
+    pages: "288",
+    categories: "Politique",
+    date: "2025",
+    image: "amerique-portrait-pays-polarise.jpg",
+  },
+  {
+    titre: "Dernière soirée",
+    auteur: "Lisa Gardner",
+    description:
+      "Le nouveau cold case de Frankie Elkin, l'épatante héroïne de Lisa Gardner. . Timothy O'Day était un pro de la randonnée en forêt. Pourtant, il y a disparu sans laisser de traces lors de son week-end d'enterrement de vie de garçon, laissant derrière lui deux parents inconsolables, une fiancée désespérée et quatre garçons d'honneur rongés par la culpabilité.. Frankie Elkin ne connaît rien à la forêt. Elle a en revanche un flair unique pour retrouver les disparus. Lorsqu'elle apprend qu'une ultime opération de recherches est organisée cinq ans après la disparition de Timothy, elle prend la route pour les montagnes du Wyoming et se joint à l'équipe. Mais à mesure que l'expédition s'enfonce dans ce territoire sauvage, il devient évident que quelqu'un est prêt à tout pour faire échouer les investigations.... Avec Frankie Elkin, Lisa Gardner compose l'un de ses plus beaux personnages de femme. Dernière soirée est une immersion dans les méandres de l'âme humaine que vous n'êtes pas près d'oublier..",
+    editeur: "Albin Michel",
+    isbn: "9782226476760",
+    pages: "457",
+    categories: "Policier",
+    date: "2025",
+    image: "derniere-soiree.jpg",
+  },
+  {
+    titre: "À qui sait attendre",
+    auteur: "Micheal Connelly",
+    description:
+      "Harry Bosch et Renée Ballard enquêtent sur des cold cases qui ont marqué la cité des anges . À la tête de l'unité des Affaires non résolues, Renée Ballard retrouve la trace, grâce à l'ADN, d'un violeur en série qui a terrorisé Los Angeles deux décennies plus tôt avant de disparaître. Mais très vite, elle va se heurter à des secrets et à des obstacles juridiques qui la forcent à demander son aide à Harry Bosch.. Et cette fois, elle pourra compter sur une autre alliée : Maddie, la fille de Harry, qui rejoint l'unité et dévoile des documents surprenants concernant l'affaire la plus emblématique du siècle dernier, celle du Dahlia noir.. Alors que la première enquête devient dangereuse, Ballard, Harry et Maddie ne seront pas de trop pour retrouver des criminels que les familles des victimes attendent depuis longtemps déjà de voir arrêtés..",
+    editeur: "Calmann-levy",
+    isbn: "9782702189047",
+    pages: "480",
+    categories: "Policier",
+    date: "2025",
+    image: "qui-sait-attendre.jpg",
+  },
+  {
+    titre: "Les fantômes de l'hôtel Jerome",
+    auteur: "John Irving",
+    description:
+      "Une saga jubilatoire et réjouissante par l'auteur de l'inoubliable Monde selon Garp. . 1941. À Aspen, Colorado, la jeune Rachel Brewster, dite « Little Ray », échoue aux épreuves de slalom mais réussit à tomber enceinte. De retour chez ses parents, elle devient monitrice de ski et élève son fils Adam dans un climat de tendre complicité. Ainsi débutent sept décennies d'une fresque débridée peuplée de personnages irrésistibles qui, dans la très conventionnelle Nouvelle-Angleterre, défient les conventions. Adulte, c'est à l'Hotel Jerome d'Aspen, hanté par de nombreux fantômes, qu'Adam tentera d'élucider les secrets bien gardés de son étonnante famille.. Paternité mystérieuse, mère « sans fil à la patte », transgressions en tous genres et sexe à tous les étages - sans oublier une savoureuse relecture de Moby-Dick : autant de thèmes où l'imagination vertigineuse et la truculence de John Irving font merveille, pour le plus grand bonheur des fans de la première heure et celui d'une nouvelle génération de lecteurs.. Après sept ans de silence, saluons le grand retour d'un romancier visionnaire qui prône depuis toujours la liberté de moeurs, la tolérance et l'amour inconditionnel..",
+    editeur: "Seuil",
+    isbn: "9782021528626",
+    pages: "983",
+    categories: "Littérature Américaine",
+    date: "2025",
+    image: "fantome-hotel-jerome.jpg",
+  },
+  {
+    titre: "La femme de ménage voit tout",
+    auteur: "Freida Mcfadden",
+    description:
+      "« Je me demande si je n'ai pas commis une terrible erreur en m'installant ici... » . Après avoir été au service des autres en tant que femme de ménage, Millie s'est enfin construit une vie à elle. Elle vient même d'emménager dans une belle maison, dans une petite impasse chic et tranquille, avec son mari et ses deux enfants.. Mais son rêve d'une vie paisible est rapidement terni par la rencontre de ses voisins. Il y a Suzette, bien trop snob et aguicheuse, son insipide mari, mais surtout leur terrifiante femme de ménage au regard perçant et au comportement plus que suspect.. Les craintes de Millie montent d'un cran lorsque des bruits étranges se font entendre la nuit dans sa propre maison. Pire : elle éprouve un étrange malaise et se sent épiée. C'est certain, quelque chose ne tourne pas rond dans cette rue si tranquille. Mais est-elle prête à en découvrir les secrets ? Et surtout, le temps de comprendre ce qui ne va pas, tout peut arriver.... Elle vous observe. Et vous n'avez nulle part où vous cacher.",
+    editeur: "City Ed.",
+    isbn: "9782824627571",
+    pages: "392",
+    categories: "Policier",
+    date: "2024",
+    image: "femme-menage-voit-tout.jpg",
+  },
+];
+
+module.exports = donneesLivres;
